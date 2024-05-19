470a71acb3e0fe633d203b4cc6aff1d513bb5375
Daichi Sato <satodai370054@gmail.com>
2024-05-19 10:53:07 +0900

    update

--- app.py
+++ app.py
@@ -10,49 +10,57 @@ import streamlit as st
 
 st.set_page_config(layout="wide")
 
-st.title("AutoML")
+st.title("AutoML for Tabular Data")
+
+# 2カラムレイアウト
 col1, col2 = st.columns([1, 1], gap="medium")
 
 col1.write("### 設定")
-uploaded_train_file = col1.file_uploader("学習データをアップロード", type="csv")
+uploaded_csv_file = col1.file_uploader("CSVファイルをアップロード", type="csv")
 
-if uploaded_train_file is not None:
-    df = pd.read_csv(uploaded_train_file)
+if uploaded_csv_file is not None:
+    df = pd.read_csv(uploaded_csv_file)
     cols = df.columns
 
     # 説明変数
-    exp_select = col1.expander("説明変数を選択")
-    selected = [True for _ in cols]
+    explanatory_cols_selector = col1.expander("説明変数を選択")
+    is_selected = [True for _ in cols]
+    
+    # デフォルトでは末尾の変数を目的変数とする
+    last_col_index = len(cols) - 1
     for i, col in enumerate(cols):
-        value = True if i < len(cols)-1 else False
-        selected[i] = exp_select.checkbox(col, value=value)
-    explanatory_cols = cols[selected].tolist()
+        value = False if i == last_col_index else True
+        is_selected[i] = explanatory_cols_selector.checkbox(col, value=value)
+    explanatory_cols: list = cols[is_selected].tolist()
 
     # 目的変数
-    target_col = col1.selectbox("目的変数を選択", cols.tolist(), index=len(cols)-1)
-
+    target_col = col1.selectbox("目的変数を選択", cols.tolist(), index=last_col_index)
     if target_col in explanatory_cols:
         col1.error(f"{target_col}は説明変数に含まれています")
 
-    # 目的変数のタスク推定
-    unique_values = np.sort(df[target_col].unique())
-    if unique_values.tolist() == list(range(len(unique_values))):
-        task = col1.selectbox("タスクを選択", ["2値分類", "回帰"])
-    else:
-        task = col1.selectbox("タスクを選択", ["回帰", "2値分類"])
+    # 使用する質的変数ごとにエンコーディングを指定
+    encoding_methods_selector = col1.expander("エンコーディングを選択")
+    use_cols = explanatory_cols + [target_col]
+    categorical_cols = df[use_cols].select_dtypes(include="object").columns.tolist()
+    encoding_methods = {}
+    for col in categorical_cols:
+        encoding_methods[col] = encoding_methods_selector.selectbox(f"{col}のエンコーディングを選択", ["LabelEncoder", "OneHotEncoder"])
     
-    # その他詳細設定
-    cv = col1.number_input('fold数', min_value=2, max_value=10, value=5, step=1)
+    # 目的変数のタスク推定
+    task = col1.selectbox("目的変数のタスクを選択", ["回帰", "分類"])
 
-    # 学習データ
-    df = df[list(set(explanatory_cols+[target_col]))]
+    # cv数
+    num_folds = col1.number_input('fold数', min_value=2, max_value=10, value=5, step=1)
+
+    # 使用データ
+    df = df[use_cols]
     col2.write("### データ")
     exp = col2.expander("データを見る")
     exp.write(f"レコード数 : {len(df)}  カラム数 : {df.shape[1]}")
     exp.write(df)
-    
-    if task != "" and col1.button("実行"):
-        if task == "回帰":
+
+    match task:
+        case "回帰":
             models = {
                 "LinearRegression": LinearRegression(),
                 "Ridge": Ridge(),
@@ -68,8 +76,9 @@ if uploaded_train_file is not None:
                 "neg_mean_squared_error": "MSE",
                 "neg_mean_absolute_error": "MAE",
             }
+            kf = KFold(num_folds, shuffle=True)
 
-        elif task == "2値分類":
+        case "分類":
             models = {
                 "LogisticRegression": LogisticRegression(),
                 "KNN": KNeighborsClassifier(),
@@ -85,10 +94,27 @@ if uploaded_train_file is not None:
                 "recall": "Recall",
                 "precision": "Precision",
             }
+            kf = StratifiedKFold(num_folds, shuffle=True)
+    
+    # モデルを選択
+    selected_models = col1.multiselect("使用するモデルを選択", list(models.keys()))
+    models = {k: v for k, v in models.items() if k in selected_models}
+
+    if task != "" and col1.button("実行"):
 
         # データ
-        df = df[list(set(explanatory_cols+[target_col]))]
-        df = df.fillna(df.mean())
+        df = df.dropna()
+
+        # エンコーディング
+        for col, method in encoding_methods.items():
+            if method == "LabelEncoder":
+                df[col] = df[col].astype("category").cat.codes
+            elif method == "OneHotEncoder":
+                df = pd.get_dummies(df, columns=[col])
+        
+        # 標準化
+        df = (df - df.mean()) / df.std()
+        
         X = df[explanatory_cols]
         y = df[target_col]
 
@@ -96,7 +122,6 @@ if uploaded_train_file is not None:
         table = []
         bar = col2.progress(0)
         latest_iteration = col2.empty()
-        kf = KFold(cv, shuffle=True) if task == "回帰" else StratifiedKFold(cv, shuffle=True)
         for i, (model_name, model) in enumerate(models.items()):
             result = cross_validate(model, X, y, cv=kf, scoring=list(metrics.keys()))
             scores = []
